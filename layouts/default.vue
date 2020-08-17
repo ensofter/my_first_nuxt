d944ab804ca125e4968bda52ec9cb21ec42f2ab1
Romashov Aleksey <amromashov@gmail.com>
2020-08-18 00:58:11 +0300

    Add Vuex

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <Toolbar/>
+      <nuxt/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Toolbar from '@/components/Toolbar.vue';
+export default {
+  components: {
+    Toolbar
+  }
+}
+</script>
