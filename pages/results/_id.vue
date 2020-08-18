7f9551a6a6d212caa610d5bd0ad2cd2dff22783e
Romashov Aleksey <amromashov@gmail.com>
2020-08-19 01:07:42 +0300

    Add routs

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,10 +1,40 @@
 <template>
-
+  <div>
+    <h1>Results for {{$router.params.id}}}</h1>
+    <div v-if="albumData">
+      <div v-for="(album, index) in albumData">
+        <Card :title="album.collectionCensoredName"
+              :image="album.artworkUrl160"
+              :artistName="album.artistName"
+              :url="album.artistViewUrl"
+              :color="picker(index)"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Card from "~/components/Card";
 export default {
-  name: "_id"
+  asyncData ({ params }) {
+    return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+      .then((response) => {
+        console.log(response.data.results);
+        return {albumData: response.data.results}
+      });
+  },
+  components: {Card},
+  methods: {
+    picker(index) {
+      return index % 2 == 0 ? 'red':'blue'
+    }
+  },
+  computed: {
+      /*albumData() {
+        return this.$store.state.albums;
+      }*/
+  },
+  middleware: 'search'
 }
 </script>
 
